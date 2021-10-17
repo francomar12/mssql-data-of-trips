61c147d9569a8f0c17a0b6c56915bf33c08c17ab
Francomar12 <francomar12@hotmail.com>
2021-10-17 16:11:29 -0600

    Checking indentation of the query

--- views/wee_trp_avg_rgn.sql
+++ views/wee_trp_avg_rgn.sql
@@ -10,10 +10,10 @@ GO
 
 CREATE VIEW [dbo].[wee_trp_avg_rgn]
 AS
-	SELECT	 region, AVG(trips) AS avg
-	FROM     (	SELECT	region, week, CAST(COUNT(1) AS DECIMAL(10, 2)) AS trips
-				FROM	(SELECT	region, origin_coord, destin_coord, datetime, data_source, DATEPART(WEEK, datetime) AS week
-						 FROM	dbo.datatrips AS trip) AS b
+	SELECT	region, AVG(trips) AS avg
+	FROM	(	SELECT	region, week, CAST(COUNT(1) AS DECIMAL(10, 2)) AS trips
+				FROM	(	SELECT	region, origin_coord, destin_coord, datetime, data_source, DATEPART(WEEK, datetime) AS week
+							FROM	dbo.datatrips AS trip) AS b
 				GROUP BY region, week) AS c
 	GROUP BY region
 GO
